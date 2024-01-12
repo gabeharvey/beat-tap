ae8f6a9e5505c310b452a441cfb3e2d785568eef
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-01-12 11:40:54 -0700

    added api calls for random artist and genre

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,7 +3,7 @@ function randomNumber(max){
     return Math.floor(Math.random()*max);
 }
 
-//function for country API
+//Function for country API
 function getRandomCountry() {
     let countryObj = {
         name: '',
@@ -21,3 +21,83 @@ function getRandomCountry() {
     });
     return countryObj;
 }
+
+//Function to generate a random genre
+function getRandomGenre(genreNumber) {
+    let randomGenre;
+    let offset = randomNumber(1800);
+    let requestURL='https://musicbrainz.org/ws/2/genre/all?limit=100&offset='+offset+'&fmt=json';
+    fetch(requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let random = randomNumber(data.genres.length);
+        randomGenre=data.genres[random].name;
+        localStorage.setItem('genre'+genreNumber,JSON.stringify(randomGenre));
+    });
+}
+
+//Function to generate a random group artist
+function getRandomArtistGroup(artistNumber) {
+    let randomArtist;
+    let offset =randomNumber(521800);
+    let requestURL='https://musicbrainz.org/ws/2/artist/?query=type:group&fmt=json&limit=100&offset='+offset;
+    fetch(requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let random = randomNumber(data.artists.length);
+        randomArtist=data.artists[random].name;
+        localStorage.setItem('artist'+artistNumber,JSON.stringify(randomArtist));
+    });
+}
+
+//Function to generate a random solo artist
+function getRandomArtistSolo(artistNumber) {
+    let randomArtist;
+    let offset =randomNumber(1234700);
+    let requestURL='https://musicbrainz.org/ws/2/artist/?query=type:person&fmt=json&limit=100&offset='+offset;
+    fetch(requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let random = randomNumber(data.artists.length);
+        randomArtist=data.artists[random].name;
+        localStorage.setItem('artist'+artistNumber,JSON.stringify(randomArtist));
+    });
+}
+
+// Function to generate a random song by country
+function getSongCountry(code,countryNumber){
+    //let offset = [];
+
+    //Get the amount of total artists in country
+    let requestURL1='https://musicbrainz.org/ws/2/artist/?query=country:'+code+'&fmt=json';
+    fetch(requestURL1)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        localStorage.setItem('offset-song-country',randomNumber(data.count));
+    });
+
+    //Get an artist in country
+    let artist;
+    let requestURL2='https://musicbrainz.org/ws/2/artist/?query=country:'+code+'&fmt=json&limit=100&offset='+localStorage.getItem('offset-song-country');
+    console.log(requestURL2);
+    fetch(requestURL2)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let random=randomNumber(data.artists.length);
+        artist=data.artists[random].name;
+        localStorage.setItem('artist-country-temp'+countryNumber,JSON.stringify(artist));
+    });
+
+}
+
+getSongCountry('GB',2);
